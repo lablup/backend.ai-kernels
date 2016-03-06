184918a8cdb8f39d5951ab38644c178d051bdf96
Joongi Kim <joongi@lablup.com>
2016-03-06 23:29:56 +0900

    refs #13: Minor fix.

--- python3/install-python-packages2.sh
+++ python3/install-python-packages2.sh
@@ -6,9 +6,6 @@ pip3 install six
 pyenv rehash
 
 cat > /tmp/matplotlib-fontcache.py <<- EOF
-import sys
-import matplotlib
-print('Matplotlib Cache Directory:', matplotlib.get_cachedir(), file=sys.stderr)
 import matplotlib.pyplot
 EOF
 python3 /tmp/matplotlib-fontcache.py
