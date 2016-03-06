c7f476f1bfc64a4b05313567e7924670ebce52d8
Joongi Kim <joongi@lablup.com>
2016-03-06 23:26:49 +0900

    refs #13: Add labels to specify different resource limits.
     * Also upgrades the version of tensorflow to the latest one.
    
     * TODO: update agent to recognize and apply these labels.

--- python3-deeplearning/install-python-packages2.sh
+++ python3-deeplearning/install-python-packages2.sh
@@ -3,7 +3,7 @@ eval "$(pyenv init -)"
 pyenv shell 3.4.4
 pip3 install networkx
 pip3 install six
-pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp34-none-linux_x86_64.whl
+pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp34-none-linux_x86_64.whl
 pyenv rehash
 
 cat > /tmp/sample_data_importer.py <<- EOF
