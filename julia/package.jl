8e573fc22984ed60f8ea1e1f3d9d51b2d0828273
Jonghyun Park <adrysn@gmail.com>
2016-10-09 19:46:06 +0900

    refs #11: add - code execution routine for Julia
    * Code delivered from ZMQ socket can be executed.
    * Code execution results (stdout, stderr) can be sent.
    * Todo: exception handling
    * Todo: graph generation
    * Todo: road testing with various kinds of Julia codes
    * Todo: code execution in jail environment
        - code can be executed with - which julia /home/sorna/run.jl - in run.sh
        - broken pipe error is raised with - exec /home/sorna/jail default which julia /home/sorna/run.jl - in run.sh

--- julia/package.jl
+++ julia/package.jl
@@ -1,1 +1,10 @@
+Pkg.init()
 Pkg.add("ZMQ")
+Pkg.add("PyPlot")
+Pkg.add("Gadfly")
+Pkg.add("JSON")
+
+using ZMQ
+using PyPlot
+using Gadfly
+import JSON
--- julia/run.jl
+++ julia/run.jl
@@ -1,33 +1,84 @@
 using ZMQ
+import JSON
 
-function main()
-    # Set home directory
-    # cd("/home/work")
 
+function parseall(code)
+    pos = start(code)
+    exs = []
+
+    while !done(code, pos)
+       ex, pos = parse(code, pos)
+       push!(exs, ex)
+    end
+
+    if length(exs) == 0
+       throw(ParseError("end of input"))
+    elseif length(exs) == 1
+       return exs[1]
+    else
+       return Expr(:block, exs...)
+    end
+end
+
+
+function execute_code(code_id, code)
     # Intercept STDOUT and STDERR
     const (OLDOUT, OLDERR) = STDOUT, STDERR
     (outRead, outWrite) = redirect_stdout()
     (errorRead, errorWrite) = redirect_stderr()
 
-    # ZMQ setup
-    ctx = Context()
-    socket = Socket(ctx, REP)
-    port = "tcp://*:2001"
-    ZMQ.bind(socket, port)
-
-    # Todo: keep reading request from ZMQ
-
-    # Todo: execute code and retrieve result / handle exceptions
+    # Julia's Base.parse() does not parse multi-line string by design
+    exprs = parseall(code)
+    eval(exprs)
 
     # Read stdout and stderr during code execution
     redirect_stdout(OLDOUT)
-    close(outWrite); out = readall(outRead); close(outRead)
+    close(outWrite); out = readstring(outRead); close(outRead)
     redirect_stderr(OLDERR)
-    close(errorWrite); err = readall(errorRead); close(errorRead)
+    close(errorWrite); err = readstring(errorRead); close(errorRead)
 
-    # Todo: reply with ZMQ
-
-    # Todo: handle termination signals
+    return out, err
 end
 
-main()
+
+# Set home directory
+cd("/home/work")
+
+# ZMQ setup
+ctx = Context()
+socket = Socket(ctx, REP)
+port = "tcp://*:2001"
+ZMQ.bind(socket, port)
+println("serving at port 2001...")
+
+try
+    while true
+        # Receive code_id
+        msg = ZMQ.recv(socket)
+        io = seek(convert(IOStream, msg), 0)
+        code_id = takebuf_string(io)
+
+        # Receive code
+        msg = ZMQ.recv(socket)
+        io = seek(convert(IOStream, msg), 0)
+        code = takebuf_string(io)
+
+        try
+            out, err = execute_code(code_id, code)
+
+            result = Dict(
+                "stdout" => out,
+                "stderr" => err,
+                "exceptions" => []
+            )
+
+            ZMQ.send(socket, Message(JSON.json(result)))
+        catch e
+            # Todo: Handle exceptions
+        end
+    end
+finally
+    ZMQ.close(socket)
+    ZMQ.close(ctx)
+    println("exit.")
+end
