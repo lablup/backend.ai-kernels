43f01d91410d27baa92afbfc6b8fa9d404646b45
Joongi Kim <joongi@lablup.com>
2016-11-02 00:13:53 +0900

    refs #19: Add a shortcut for pretty graph-view log command.

--- git/run.sh
+++ git/run.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 eval "$(pyenv init -)"
 pyenv shell $SORNA_PYTHON_VERSION
+git config --global alias.pretty-log 'log --graph --branches --tags --oneline --decorate'
 exec /home/sorna/jail git `pyenv which python` /home/sorna/run.py
