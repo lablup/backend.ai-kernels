0afeb6c9bb09f86eb2a851a7da5dbda0fd829214
Jonghyun Park <adrysn@gmail.com>
2016-11-02 16:43:46 +0900

    refs #18: change - appropriate names for ExecResult's fields / Create a dummy ExceptionInfo type

--- haskell/run.hs
+++ haskell/run.hs
@@ -12,13 +12,23 @@ import qualified Data.ByteString.UTF8 as U
 -- Define execution result data type.
 -- Map type cannot contain heterogeneous data.
 data ExecResult = ExecResult {
-    _stdout :: String,
-    _stderr :: String,
-    _exceptions :: [String]
+    stdout :: String,
+    stderr :: String,
+    exceptions :: [Maybe String]
 } deriving (Generic)
 instance ToJSON ExecResult
 instance FromJSON ExecResult
 
+-- Exception information
+data ExceptionInfo = ExceptionInfo {
+    exc :: String,
+    args :: [String],
+    raisedBeforeExec :: Bool,
+    traceback :: Maybe String
+}
+-- TODO: Serializer for ExceptionInfo type
+
+main :: IO ()
 main = do
     let
     putStrLn "serving at port 2001..."
@@ -45,6 +55,6 @@ lazyToStrict :: BL.ByteString -> B.ByteString
 lazyToStrict = B.concat . BL.toChunks
 
 -- Execute code and give results
-executeCode :: String -> String -> (String, String, [String])
-executeCode code_id code = ("fake out", "fake err", ["fake exceptions"])
+executeCode :: String -> String -> (String, String, [Maybe String])
+executeCode code_id code = ("fake out", "fake err", [])
 -- TODO: real code execution routine
