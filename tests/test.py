4d4de3ebbf97acaeafb1a80618e9b1a8aa0aab19
Joongi Kim <joongi@lablup.com>
2016-11-10 22:19:08 +0900

    refs #24: Remove warnings when testing latest TensorFlow images.
     * Also ensure that the GPU image test uses the first GPU always.

--- tests/test.py
+++ tests/test.py
@@ -275,9 +275,8 @@ with tf.device('/cpu:0'):
     loss = tf.reduce_mean(tf.square(y - y_data))
     optimizer = tf.train.GradientDescentOptimizer(0.5)
     train = optimizer.minimize(loss)
-    init = tf.initialize_all_variables()
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-    sess.run(init)
+    sess.run(tf.global_variables_initializer())
     for step in range(201):
         sess.run(train)
         if step % 20 == 0:
@@ -301,21 +300,20 @@ import tensorflow as tf
 import numpy as np
 x_data = np.random.rand(100).astype(np.float32)
 y_data = x_data * 0.1 + 0.3
-#with tf.device('/gpu:0'):
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.zeros([1]))
-y = W * x_data + b
-loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
-init = tf.initialize_all_variables()
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-sess.run(init)
-for step in range(201):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b))
-sess.close()
+with tf.device('/gpu:0'):
+    W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    b = tf.Variable(tf.zeros([1]))
+    y = W * x_data + b
+    loss = tf.reduce_mean(tf.square(y - y_data))
+    optimizer = tf.train.GradientDescentOptimizer(0.5)
+    train = optimizer.minimize(loss)
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    sess.run(tf.global_variables_initializer())
+    for step in range(201):
+        sess.run(train)
+        if step % 20 == 0:
+            print(step, sess.run(W), sess.run(b))
+    sess.close()
 print('done')
 '''
 
