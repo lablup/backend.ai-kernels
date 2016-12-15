17cc5e2409979d34584708483592b0be1c365fc2
Joongi Kim <joongi@lablup.com>
2016-12-15 18:52:04 +0900

    refs #28: Apply apt options to exclude recommended/suggested packages.
     * The option mostly excludes documentation packages.
    
     * The size of Python 2/3 images is reduced by about 200 to 250 MBytes.

--- python2/install-python.sh
+++ python2/install-python.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 eval "$(pyenv init -)"
-pyenv install 2.7.12
-pyenv shell 2.7.12
+pyenv install $SORNA_PYTHON_VERSION
+pyenv shell $SORNA_PYTHON_VERSION
 pyenv rehash
 pip install --upgrade -q pip
 pip install wheel
--- python2/run.sh
+++ python2/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 eval "$(pyenv init -)"
-pyenv shell 2.7.12
+pyenv shell $SORNA_PYTHON_VERSION
 exec /home/sorna/jail python `pyenv which python` /home/sorna/run.py
