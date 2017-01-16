3218122d9305fc6ce16c3cf84fe5e532d9f14438
Jeongkyu Shin <inureyes@gmail.com>
2017-01-16 20:30:26 +0900

    refs #10 : update - use octave-zeromq.  * add - working prototype REPL loop for octave

---
+++ octave4/run.m
@@ -0,0 +1,24 @@
+pkg load zeromq
+
+function [stdout, stderr, media, exceptions] = execute_code(code)
+  stdout = [];
+  stderr = [];
+  media = [];
+  exceptions = [];
+  try
+    eval(code);
+  catch
+    exceptions = lasterror.message;
+  end_try_catch
+endfunction
+
+sock = zmq_socket(ZMQ_PULL);
+zmq_bind(sock, "tcp://*:2001");
+printf ("serving at port 2001...")
+
+%cd /home/work;
+while(true)
+  codeid = zmq_recv(sock, 100, 0);
+  code = zmq_recv(sock, 100000, 0);
+  [stdout, stderr, media, exceptions] = execute_code(char(code));
+endwhile
