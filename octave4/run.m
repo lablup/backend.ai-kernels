63b2a91f241f28b46fc4f69326bbdc8ae08edbce
Jeongkyu Shin <inureyes@gmail.com>
2017-01-19 12:23:55 +0900

    refs #10 : update - let docker compile octave 4.2

--- octave4/run.m
+++ octave4/run.m
@@ -1,6 +1,6 @@
 pkg load zeromq
 
-addpath('./jsonlab-master');
+addpath('/home/sorna/jsonlab-master');
 % Override memory specific function
 function clear (varargin)
   %for i = 1:length(varargin)
@@ -14,11 +14,12 @@ function result = execute_code(code)
   media = [];
   exceptions = [];
   try
-    evalc(code);
+    stdout = evalc(code);
     if strfind(code, "plot") || strfind(code, "figure")
       print("octave_figure_internal.svg");
       fstr = fileread("octave_figure_internal.svg");
-      media = fstr;
+      media = {"image/svg+xml", fstr};
+      unlink("octave_figure_internal.svg")
     endif
   catch
     exceptions = lasterror.message;
@@ -29,8 +30,9 @@ function result = execute_code(code)
   result.exceptions = exceptions;
 endfunction
 
-sock = zmq_socket(ZMQ_PULL);
+sock = zmq_socket(ZMQ_REP);
 zmq_bind(sock, "tcp://*:2001");
+printf (["Octave version : ", version])
 printf ("serving at port 2001...")
 
 %cd /home/work;
@@ -38,4 +40,7 @@ while(true)
   codeid = zmq_recv(sock, 100, 0);
   code = zmq_recv(sock, 100000, 0);
   result = execute_code(char(code));
+  a = savejson('', result);
+  zmq_send(sock, a)
+  printf (a)
 endwhile
