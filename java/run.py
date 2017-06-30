16e4e0aeb4f46c5dc4f032761e071b255cadac61
Jonghyun Park <adrysn@gmail.com>
2017-06-30 10:21:38 +0900

    refs #46: update - run.py to work with base_run.py

--- java/run.py
+++ java/run.py
@@ -1,138 +1,102 @@
-#! /usr/bin/env python
-import asyncio
+#! /usr/bin/env python3
+
 import logging
 import os
 import re
-import signal
+from pathlib import Path
+import shlex
 import sys
 import tempfile
 
-import uvloop
-import zmq, aiozmq
-
+sys.path.insert(0, os.path.abspath('.'))
+from base_run import BaseRunner
+# For debugging
+# sys.path.insert(0, os.path.abspath('..'))
+# from base.run import BaseRunner
 
 log = logging.getLogger()
 
-cmdspec = 'javac {mainpath} && java {filename}'
+JCC = 'javac'
+JCR = 'java'
+DEFAULT_JFLAGS = '-d .'
+CHILD_ENV = {
+    'TERM': 'xterm',
+    'LANG': 'C.UTF-8',
+    'SHELL': '/bin/ash',
+    'USER': 'work',
+    'HOME': '/home/work',
+    'PATH': '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/jdk/bin',
+}
 
 
-'''
-A thin wrapper for an external command.
+class JavaProgramRunner(BaseRunner):
 
-It creates a temporary file with user code, compile and run it, and
-returns the outputs of the execution.
-'''
-async def execute(insock, outsock, code_id, code_data):
-    loop = asyncio.get_event_loop()
+    log_prefix = 'java-kernel'
 
-    # Parse public class name
-    # If public class exists in source file, get the name. The name of the
-    # file and the (unique) public class name should match in Java. This
-    # approach may not be perfect, so other parsing strategy should be applied
-    # in the future.
-    m = re.search('public[\s]+class[\s]+([\w]+)[\s]*{',
-                  code_data.decode('ascii'))
-    filename = None
-    if m and len(m.groups()) > 0:
-        filename = '/home/work/' + m.group(1) + '.java'
+    def __init__(self):
+        super().__init__()
+        self.child_env = CHILD_ENV
 
-    # Save code to a temporary file
-    if filename:
-        tmpf = open(filename, 'w+b')
-    else:
-        tmpf = tempfile.NamedTemporaryFile(suffix='.java', dir='.')
-    tmpf.write(code_data)
-    tmpf.flush()
+    async def build(self, build_cmd):
+        if build_cmd is None or build_cmd == '':
+            # skipped
+            return
+        elif build_cmd == '*':
+            # use the default heuristic
+            if Path('makefile').is_file():
+                await self.run_subproc('make')
+            else:
+                javafiles = Path('.').glob('**/*.java')
+                javafiles = ' '.join(map(lambda p: shlex.quote(str(p)), javafiles))
+                cmd = f'{JCC} {DEFAULT_JFLAGS} {javafiles}'
+                await self.run_subproc(cmd)
+        else:
+            await self.run_subproc(build_cmd)
 
-    try:
-        # Compile and run saved code.
-        proc = await asyncio.create_subprocess_shell(
-            cmdspec.format(mainpath=tmpf.name,
-                           filename=tmpf.name.split('/')[-1][:-5]),
-            stdin=None,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE
-        )
-        pipe_tasks = [
-            loop.create_task(pipe_output(proc.stdout, outsock, 'stdout')),
-            loop.create_task(pipe_output(proc.stderr, outsock, 'stderr')),
-        ]
-        await proc.wait()
-        for t in pipe_tasks:
-            t.cancel()
-            await t
-    except:
-        log.exception()
-    finally:
-        # Close and delete the temporary file.
-        tmpf.close()
+    async def execute(self, exec_cmd):
+        if exec_cmd is None or exec_cmd == '':
+            # skipped
+            return
+        elif exec_cmd == '*':
+            if Path('./Main.java').is_file():
+                await self.run_subproc(f'{JCR} Main')
+            else:
+                log.error('cannot distinguish entry class (Main.class).')
+        else:
+            # TODO: enable user-specified execution options.
+            await self.run_subproc(exec_cmd)
+
+    async def query(self, code_text):
+        # Parse public class name
+        # If public class exists in source file, get the name. The name of the
+        # file and the (unique) public class name should match in Java. This
+        # approach may not be perfect, so other parsing strategy should be
+        # applied in the future.
+        m = re.search('public[\s]+class[\s]+([\w]+)[\s]*{', code_text)
+        filename = None
+        if m and len(m.groups()) > 0:
+            filename = f'/home/work/' + m.group(1) + '.java'
+
+        # Save code to a temporary file
         if filename:
-            os.remove(filename)
+            tmpf = open(filename, 'w+b')
+        else:
+            tmpf = tempfile.NamedTemporaryFile(suffix='.java', dir='.')
+        tmpf.write(code_text.encode('utf8'))
+        tmpf.flush()
 
-
-async def pipe_output(stream, outsock, target):
-    assert target in ('stdout', 'stderr')
-    try:
-        while True:
-            data = await stream.read(4096)
-            if not data:
-                break
-            outsock.write([target.encode('ascii'), data])
-            await outsock.drain()
-    except (aiozmq.ZmqStreamClosed, asyncio.CancelledError):
-        pass
-
-async def main_loop():
-    insock  = await aiozmq.create_zmq_stream(zmq.PULL, bind='tcp://*:2000')
-    outsock = await aiozmq.create_zmq_stream(zmq.PUSH, bind='tcp://*:2001')
-    print('start serving...')
-    while True:
         try:
-            data = await insock.read()
-            code_id = data[0].decode('ascii')
-            code_data = data[1]
-            await execute(insock, outsock, code_id, code_data)
-            outsock.write([b'finished', b''])
-            await outsock.drain()
-        except asyncio.CancelledError:
-            break
-        except:
-            log.exception()
-            break
-    insock.close()
-    outsock.close()
+            cmd = f'{JCC} {filename} && {JCR} {filename.split("/")[-1][:-5]}'
+            await self.run_subproc(cmd)
+        finally:
+            # Close and delete the temporary file.
+            tmpf.close()
+            if filename:
+                os.remove(filename)
 
 
 def main():
-    # Replace stdin with a "null" file
-    # (trying to read stdin will raise EOFError immediately afterwards.)
-    sys.stdin = open(os.devnull, 'rb')
-
-    # Initialize event loop.
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-    loop = asyncio.get_event_loop()
-    stopped = asyncio.Event()
-
-    def interrupt(loop, stopped):
-        if not stopped.is_set():
-            stopped.set()
-            loop.stop()
-        else:
-            print('forced shutdown!', file=sys.stderr)
-            sys.exit(1)
-
-    loop.add_signal_handler(signal.SIGINT, interrupt, loop, stopped)
-    loop.add_signal_handler(signal.SIGTERM, interrupt, loop, stopped)
-
-    try:
-        main_task = loop.create_task(main_loop())
-        loop.run_forever()
-        # interrupted
-        main_task.cancel()
-        loop.run_until_complete(main_task)
-    finally:
-        loop.close()
-        print('exit.')
+    JavaProgramRunner().run()
 
 if __name__ == '__main__':
     main()
