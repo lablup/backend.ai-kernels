de4517647896db7fb758c8f9721685f17ee216ce
Joongi Kim <joongi@lablup.com>
2017-09-05 23:36:48 +0900

    refs #19: Update Git kernel using the new base images

--- git/install-python.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-eval "$(pyenv init -)"
-export PYTHON_CONFIGURE_OPTS="--enable-shared"
-pyenv install $SORNA_PYTHON_VERSION
-pyenv shell $SORNA_PYTHON_VERSION
-pyenv rehash
-pip3 install -U -q pip wheel setuptools
