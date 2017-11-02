3f775929881bebd5d03c48c300ec21b471ee9ac7
Joongi Kim <joongi@lablup.com>
2017-11-03 01:13:21 +0900

    base: Always perform ldconfig right before running the command
     * There are libraries that are mounted at the time of container
       creation, such as NVIDIA driver and CUDA libraries.
    
       Even though we add explicit /etc/ld.conf.so.d/*.conf when building
       the images of GPU-enabled kernels, ldconfig run in the build step
       does not have effects as those paths do NOT exist during builds.
       We should run ldconfig AFTER we have the actual library files.

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Add local user
-# Either use the LOCAL_USER_ID if passed in at runtime or
-# fallback
-
 USER_ID=${LOCAL_USER_ID:-9001}
 USER_NAME=work
 
@@ -11,5 +7,9 @@ echo "Starting with user $USER_NAME ($USER_ID)"
 useradd -s /bin/ash -d "/home/$USER_NAME" -M -r -u $USER_ID -U -o -c "User" $USER_NAME
 chown -R work:work /home/sorna
 export HOME="/home/$USER_NAME"
+
+# To use libraries mounted at container creation
+# e.g., nvidia driver and cuda libraries
+ldconfig
 
 exec su-exec $USER_NAME "$@"
