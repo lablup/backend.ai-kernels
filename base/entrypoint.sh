a0f300ec9266f663d3639ff6c39f4e130d3456bb
Joongi Kim <joongi@lablup.com>
2017-11-04 00:35:32 +0900

    base: Mitigate rare fs-sync bug
     * There may be related to a long-standing Docker issue: moby/moby#9547
    
     * The original bug is about chmod, but I have seen the same error
       during tests...

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -6,6 +6,8 @@ USER_NAME=work
 echo "Starting with user $USER_NAME ($USER_ID)"
 useradd -s /bin/ash -d "/home/$USER_NAME" -M -r -u $USER_ID -U -o -c "User" $USER_NAME
 chown -R work:work /home/sorna
+sync
+
 export HOME="/home/$USER_NAME"
 
 exec su-exec $USER_NAME "$@"
