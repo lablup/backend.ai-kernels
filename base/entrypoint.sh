02b3268bb96e3faf4dc8649498716aad2b7b097a
Joongi Kim <joongi@lablup.com>
2017-11-06 17:45:24 +0900

    Minor fix
     * Let entrypoint.sh to detect ash/bash

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -4,7 +4,11 @@ USER_ID=${LOCAL_USER_ID:-9001}
 USER_NAME=work
 
 echo "Starting with user $USER_NAME ($USER_ID)"
-useradd -s /bin/ash -d "/home/$USER_NAME" -M -r -u $USER_ID -U -o -c "User" $USER_NAME
+if [ -f /bin/ash ]; then  # for alpine
+  useradd -s /bin/ash -d "/home/$USER_NAME" -M -r -u $USER_ID -U -o -c "User" $USER_NAME
+else
+  useradd -s /bin/bash -d "/home/$USER_NAME" -M -r -u $USER_ID -U -o -c "User" $USER_NAME
+fi
 chown -R work:work /home/sorna
 sync
 
