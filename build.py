c9c76ba18a8c4be498fc096c142bce38e02c49dd
Joongi Kim <joongi@lablup.com>
2018-10-01 16:01:09 +0900

    script: Remove latest tagging
    * "latest" tags are now expressed as "aliases" in Backend.AI's etcd
      configuration where users/operators can control which particular
      version of image is their own "latest"

--- build.py
+++ build.py
@@ -23,28 +23,17 @@ def print_header(s):
     print(_hdr_color + s + _reset_color)
 
 
-def build_kernel(name, tag, extra_opts='', *, latest=False, squash=False):
+def build_kernel(name, tag, extra_opts='', *, squash=False):
     assert Path(name).is_dir()
 
     sq = '--squash' if squash else ''
-    latest_tag = 'latest'
-    if 'dense' in tag:
-        latest_tag += '-dense'
-    if 'gpu' in tag:
-        latest_tag += '-gpu'
 
-    print_header(f'Building {name}' + ' (latest)' if latest else '')
+    print_header(f'Building {name}')
     run('docker build '
         f'-t lablup/kernel-{name}:{tag} {extra_opts} '
         f'-f {name}/Dockerfile.{tag} {sq} {name}')
-    if latest:
-        run('docker tag '
-            f'lablup/kernel-{name}:{tag} '
-            f'lablup/kernel-{name}:{latest_tag}')
     if auto_push:
         run(f'docker push lablup/kernel-{name}:{tag}')
-        if latest:
-            run(f'docker push lablup/kernel-{name}:{latest_tag}')
 
 
 def build_common(name, tag, extra_opts=''):
@@ -61,46 +50,46 @@ build_kernel('base', 'hook')
 build_kernel('base', 'jail-alpine')
 build_kernel('base', 'hook-alpine')
 
-build_kernel('base', 'debian', latest=True)
+build_kernel('base', 'debian')
 build_kernel('base', 'alpine')
-build_kernel('base', 'ubuntu', latest=True)
-build_kernel('base', 'conda', latest=True)
+build_kernel('base', 'ubuntu')
+build_kernel('base', 'conda')
 build_kernel('base', 'ubuntu16.04-mkl2018.3')
 
-build_kernel('base-python-minimal', '3.6-debian', squash=True, latest=True)
+build_kernel('base-python-minimal', '3.6-debian', squash=True)
 build_kernel('base-python-wheels',  '3.6-alpine')
 build_kernel('base-python-minimal', '3.6-alpine', squash=True)
-build_kernel('base-python-minimal', '3.6-ubuntu', squash=True, latest=True)
-build_kernel('base-python-minimal', '3.6-conda', squash=True, latest=True)
-build_kernel('python',              '3.6-debian', squash=True, latest=True)
-build_kernel('python',              '3.6-ubuntu', squash=True, latest=True)
-build_kernel('python',              '3.6-conda', squash=True, latest=True)
-# TODO: (kernel-runner update required) build_kernel('base-python-minimal', '2.7-debian', squash=True, latest=True)
+build_kernel('base-python-minimal', '3.6-ubuntu', squash=True)
+build_kernel('base-python-minimal', '3.6-conda', squash=True)
+build_kernel('python',              '3.6-debian', squash=True)
+build_kernel('python',              '3.6-ubuntu', squash=True)
+build_kernel('python',              '3.6-conda', squash=True)
+# TODO: (kernel-runner update required) build_kernel('base-python-minimal', '2.7-debian', squash=True)
 # TODO: (kernel-runner update required) build_kernel('base-python-wheels',  '2.7-alpine')
 # TODO: (kernel-runner update required) build_kernel('base-python-minimal', '2.7-alpine', squash=True)
-# TODO: (kernel-runner update required) build_kernel('python',              '2.7-debian', squash=True, latest=True)
+# TODO: (kernel-runner update required) build_kernel('python',              '2.7-debian', squash=True)
 
-build_kernel('git',     'alpine', squash=True, latest=True)
-build_kernel('c',       'gcc6.3-alpine', latest=True)
-build_kernel('cpp',     'gcc6.3-alpine', latest=True)
-build_kernel('java',    '8-alpine',      latest=True)
-build_kernel('rust',    '1.17-alpine',   latest=True)
-build_kernel('go',      '1.9-alpine',    latest=True)
+build_kernel('git',     'alpine', squash=True)
+build_kernel('c',       'gcc6.3-alpine')
+build_kernel('cpp',     'gcc6.3-alpine')
+build_kernel('java',    '8-alpine')
+build_kernel('rust',    '1.17-alpine')
+build_kernel('go',      '1.9-alpine')
 build_kernel('go',      '1.8-alpine')
-#build_kernel('haskell', 'ghc8.2-debian', latest=True)
-build_kernel('lua',     '5.3-alpine', latest=True)
+#build_kernel('haskell', 'ghc8.2-debian')
+build_kernel('lua',     '5.3-alpine')
 build_kernel('lua',     '5.2-alpine')
 build_kernel('lua',     '5.1-alpine')
-build_kernel('php',     '7-alpine',   latest=True)
-build_kernel('nodejs',  '6-alpine',   latest=True)
-build_kernel('nodejs',  '8-alpine',   latest=True)
-build_kernel('nodejs',  '9-alpine',   latest=True)
-build_kernel('julia',   '0.6-debian', latest=True)
-build_kernel('r',       '3.3-alpine', latest=True)
-# TODO: (not modernized) build_kernel('octave',  '4.2-debian',   latest=True)
-# TODO: (not implemented) build_kernel('swift',  'XX-alpine', latest=True)
+build_kernel('php',     '7-alpine')
+build_kernel('nodejs',  '6-alpine')
+build_kernel('nodejs',  '8-alpine')
+build_kernel('nodejs',  '9-alpine')
+build_kernel('julia',   '0.6-debian')
+build_kernel('r',       '3.3-alpine')
+# TODO: (not modernized) build_kernel('octave',  '4.2-debian')
 # TODO: (not implemented) build_kernel('swift',  'XX-alpine')
-# TODO: (not implemented) build_kernel('mono',   'XX-alpine', latest=True)
+# TODO: (not implemented) build_kernel('swift',  'XX-alpine')
+# TODO: (not implemented) build_kernel('mono',   'XX-alpine')
 # TODO: (not implemented) build_kernel('mono',   'XX-alpine')
 
 
@@ -127,10 +116,10 @@ build_common('tensorflow', '1.5-py36-gpu')
 #build_common('tensorflow', '1.3-py36')
 #build_common('tensorflow', '1.3-py36-gpu')
 #
-build_kernel('python-tensorflow', '1.8-py36', latest=True)
-build_kernel('python-tensorflow', '1.8-py36-gpu', latest=True)
-build_kernel('python-tensorflow', '1.7-py36', latest=True)
-build_kernel('python-tensorflow', '1.7-py36-gpu', latest=True)
+build_kernel('python-tensorflow', '1.8-py36')
+build_kernel('python-tensorflow', '1.8-py36-gpu')
+build_kernel('python-tensorflow', '1.7-py36')
+build_kernel('python-tensorflow', '1.7-py36-gpu')
 build_kernel('python-tensorflow', '1.6-py36')
 build_kernel('python-tensorflow', '1.6-py36-gpu')
 build_kernel('python-tensorflow', '1.5-py36')
@@ -140,16 +129,16 @@ build_kernel('python-tensorflow', '1.5-py36-gpu')
 #build_kernel('python-tensorflow', '1.3-py36')
 #build_kernel('python-tensorflow', '1.3-py36-gpu')
 
-build_kernel('python-caffe',      '1.0-py36', latest=True, squash=True)
-# TODO: (GPU not implemented) build_kernel('python-caffe',      '1.0-py36-gpu', squash=True, latest=True)
+build_kernel('python-caffe',      '1.0-py36', squash=True)
+# TODO: (GPU not implemented) build_kernel('python-caffe',      '1.0-py36-gpu', squash=True)
 # TODO: (not implemented) build_kernel('python-caffe2',     '0.8-py36', squash=True)
-# TODO: (not implemented) build_kernel('python-caffe2',     '0.8-py36-gpu', squash=True, latest=True)
-build_kernel('python-torch',      '0.2-py36', latest=True, squash=True)
-build_kernel('python-torch',      '0.2-py36-gpu', latest=True, squash=True)
+# TODO: (not implemented) build_kernel('python-caffe2',     '0.8-py36-gpu', squash=True)
+build_kernel('python-torch',      '0.2-py36', squash=True)
+build_kernel('python-torch',      '0.2-py36-gpu', squash=True)
 # TODO (not modernized): build_kernel('python-theano',     '0.2-py36', squash=True)
-# TODO (not modernized): build_kernel('python-theano',     '0.2-py36-gpu', squash=True, latest=True)
+# TODO (not modernized): build_kernel('python-theano',     '0.2-py36-gpu', squash=True)
 
 # CNTK image (currently draft version base on Ubuntu:16.04)
-build_kernel('python-cntk', '2.2-py36', latest=True, squash=True)
+build_kernel('python-cntk', '2.2-py36', squash=True)
 
 build_kernel('vendor/aws_polly', 'debian')
