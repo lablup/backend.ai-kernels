56a332303375c641f89708b4e522c5403384a782
Mario Cho <hephaex@gmail.com>
2019-05-23 23:29:40 +0900

    add NGC NVCaffe (python2)

--- build.py
+++ build.py
@@ -310,6 +310,8 @@ def main(build, list_builds, _auto_push):
         
         build_kernel('vendor/ngc-mxnet',  '19.04-mnxet')
 
+        build_kernel('vendor/ngc-NVCaffe', '19.04-NVCaffe')
+        
         build_kernel('vendor/ngc-rapids',  '0.5-rapids-py3')
         build_kernel('vendor/ngc-chainer', '4.0-py3')
 
