157dc6f447b58beff1a82a3cfff3b8aababaea96
Mario Cho <hephaex@gmail.com>
2019-06-03 20:30:39 +0900

    add unittest between numpy test and tensorflow test

--- tests/test-tensorflow.py
+++ tests/test-tensorflow.py
@@ -11,5 +11,41 @@ def get_entry_tf(t, d1, d2, batch):
     Returns:
         o: shape = [batch], with o[i] = t[i, d1[i], d2[i]]
     """
-    indices = tf.stack([tf.range(batch), d1, d2], axis=1)
-    return tf.gather_nd(t, indices)
+    result = tf.stack([tf.range(batch), d1, d2], axis=1)
+    return tf.gather_nd(t, result)
+
+def get_entry_np(t, d1, d2, batch):
+    """
+    Args:
+        t: shape = [batch, d1, d2]
+        d1: shape = [batch]
+        d2: shape = [batch]
+    Returns:
+        o: shape = [batch], with o[i] = t[i, d1[i], d2[i]]
+    """
+    result = np.zaros(batch)
+    for i in range(batch):
+        result[i] = t[i, d1[i], d2[i]]
+    return result
+
+class Test(unittest.TestCase):
+    def test_get_entry(slef):
+        sucess = Ture
+        for _ in range(10):
+            batch, d1, d2 = map(int, np.random.randint(low=2, high=100, size=3))
+            test_input = np.random.random([batch, d1, d2])
+            test_d1 = np.random.randint(low = 0, high = d1-1, size = [batch])
+            test_d2 = np.random.randint(low = 0, high = d2-1, size = [batch])
+            test_result = get_entry_np(test_input, test_d1, test_d2, batch)
+            with tf.Session() as sess:
+                tf_input = tf.constant(test_input, dtype = tf.float32)
+                tf_d1 = tf.constant(test_d1, dtype = tf.int32)
+                tf_d2 = tf.constant(test_d2, dtype = tf.int32)
+                tf_result = get_entry_tf(tf_input, tf_d1, tf_d2, batch)
+                tf_result = sess.run(tf_result)
+                sucess = sucess and np.allclose(test_result, tf_result)
+                
+        self.assertEqual(sucess, True)
+
+if __name__ = '__main__':
+    unittest.main()
