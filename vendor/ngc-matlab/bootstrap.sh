63e980ecb5d467326377715bfc8661913a4df12b
Joongi Kim <joongi@lablup.com>
2019-10-22 06:01:08 +0900

    ngc-matlab: It works only if client proxy host/port must match
    * We need to vncserver/novnc to *ignore* host/port differences...

--- vendor/ngc-matlab/bootstrap.sh
+++ vendor/ngc-matlab/bootstrap.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
 # reference: /bin/run.sh
 
-SUEXEC="/opt/kernel/su-exec $USER_ID:$GROUP_ID"
-
+echo "Setting up noVNC auto-connection"
 rm /opt/noVNC/index.html
 ln -s /opt/noVNC/redirect.html /opt/noVNC/index.html
 
 rm -rf /tmp/.X*
 
-"$SUEXEC" /usr/bin/vncserver -geometry 1600x1200 > /dev/null 2>&1
+echo "Starting background VNC server"
+export PASSWORD=matlab
+printf "${PASSWORD}\n${PASSWORD}\n\n" | /opt/kernel/su-exec $LOCAL_USER_ID:$LOCAL_GROUP_ID vncpasswd
+
+ls -al /home/work
+/opt/kernel/su-exec $LOCAL_USER_ID:$LOCAL_GROUP_ID /usr/bin/vncserver -geometry 1600x1200
+
+echo "VNC server is started."
 
 # The next line is executed by the kernel runner's service port handler.
 # "$SUEXEC" /opt/noVNC/utils/launch.sh --listen 6080 --vnc localhost:5901 > /dev/null 2>&1 &
