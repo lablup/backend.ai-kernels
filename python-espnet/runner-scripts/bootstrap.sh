c6592fadbe273e39e380969aac246740c355c2eb
Mario Cho <hephaex@gmail.com>
2021-03-29 11:53:01 +0900

    delete custom ssh port

--- python-espnet/runner-scripts/bootstrap.sh
+++ python-espnet/runner-scripts/bootstrap.sh
@@ -4,9 +4,4 @@ cp -rp /home/work/id_container /home/work/.ssh/id_rsa
 cat /home/work/.ssh/authorized_keys > /home/work/.ssh/id_rsa.pub
 chown work.work /home/work/.ssh/id_rsa.pub
 chmod 644 /home/work/.ssh/id_rsa.pub
-# Default ssh client config
-cat <<EOF >> /home/work/.ssh/config
-Port 2200
-EOF
-chown work.work /home/work/.ssh/config
 
