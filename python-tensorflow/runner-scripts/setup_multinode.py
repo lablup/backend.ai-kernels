cfb05071eb3fb38178d0dd1023d4df91b4a94984
Jeongkyu Shin <inureyes@gmail.com>
2021-07-22 13:43:05 +0900

    fix: support both old and new distributed TensorFlow

--- python-tensorflow/runner-scripts/setup_multinode.py
+++ python-tensorflow/runner-scripts/setup_multinode.py
@@ -6,20 +6,17 @@ if 'BACKENDAI_CLUSTER_HOST' in os.environ: # Start mutli-instance setup.
     env['cluster'] = {}
     env['cluster']['worker'] = []
     env['cluster']['chief'] = [] # For compatibility 
-    env['cluster']['master'] = [] # For compatibility 
-    env['cluster']['ps'] = [] # For compatibility 
     env['cluster']['chief'].append('main1:2220')
-    env['cluster']['master'].append('main1:2220')
-    env['cluster']['ps'].append('main1:2220')
     for container in os.environ['BACKENDAI_CLUSTER_HOSTS'].split(","):
-        env['cluster']['worker'].append(container + ":2220")
+        if container != 'main1':
+            env['cluster']['worker'].append(container + ":2220")
     env['task'] = {}
     if os.environ['BACKENDAI_CLUSTER_ROLE'] == 'main':
-        env['task']['type'] = "worker" # Was chief. but recent TF choose first worker as chief.
+        env['task']['type'] = "chief" # For compatibility. Recent TF choose first worker as chief.
         env['task']["index"] = str(int(os.environ['BACKENDAI_CLUSTER_IDX']) - 1) # Index starts from 0
     else:
         env['task']['type'] = "worker"
-        env['task']["index"] = os.environ['BACKENDAI_CLUSTER_IDX'] 
+        env['task']["index"] = str(int(os.environ['BACKENDAI_CLUSTER_IDX']) - 1) # Index starts from 0
     print(json.dumps(env))
 else:
     print("")
