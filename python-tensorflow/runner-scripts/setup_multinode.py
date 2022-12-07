908ccc1f047b501b2d62051278d0cf1a3a333944
Joongi Kim <joongi@lablup.com>
2022-12-07 14:51:59 +0000

    feat: Support host-mode networking in automatic TF_CONFIG

--- python-tensorflow/runner-scripts/setup_multinode.py
+++ python-tensorflow/runner-scripts/setup_multinode.py
@@ -1,22 +1,50 @@
 import os
 import json
+from pathlib import Path
 
-if 'BACKENDAI_CLUSTER_HOST' in os.environ: # Start mutli-instance setup.
-    env = {}
-    env['cluster'] = {}
-    env['cluster']['worker'] = []
-    env['cluster']['chief'] = [] # For compatibility 
-    env['cluster']['chief'].append('main1:2220')
-    for container in os.environ['BACKENDAI_CLUSTER_HOSTS'].split(","):
-        if container != 'main1':
-            env['cluster']['worker'].append(container + ":2220")
-    env['task'] = {}
-    if os.environ['BACKENDAI_CLUSTER_ROLE'] == 'main':
-        env['task']['type'] = "chief" # For compatibility. Recent TF choose first worker as chief.
-        env['task']["index"] = str(int(os.environ['BACKENDAI_CLUSTER_IDX']) - 1) # Index starts from 0
+
+def get_ssh_port(mapping, cluster_host):
+    if mapping is None:
+        return 2220
+    try:
+        _, port = mapping[cluster_host]  # host-ip and port
+        return port
+    except KeyError:
+        return 2220
+
+
+if __name__ == "__main__":
+    if "BACKENDAI_CLUSTER_HOST" in os.environ: # Start mutli-instance setup.
+        try:
+            ssh_port_mapping = json.loads(Path("/home/config/ssh/port-mapping.json").read_bytes())
+        except FileNotFoundError:
+            ssh_port_mapping = None
+
+        env = {
+            "cluster": {
+                "chief": ["main1:" + get_ssh_port(ssh_port_mapping, "main1")],
+                "worker": [],
+            },
+        }
+        for cluster_host in os.environ["BACKENDAI_CLUSTER_HOSTS"].split(","):
+            if cluster_host != "main1":
+                env["cluster"]["worker"].append(
+                    cluster_host + ":" + get_ssh_port(ssh_port_mapping, cluster_host)
+                )
+
+        # TF's worker index starts from 0.
+        cluster_role_idx = str(int(os.environ["BACKENDAI_CLUSTER_IDX"]) - 1)
+        if os.environ["BACKENDAI_CLUSTER_ROLE"] == "main":
+            task = {
+                "type": "chief",
+                "index": cluster_role_idx,
+            }
+        else:
+            task = {
+                "type": "worker",
+                "index": cluster_role_idx,
+            }
+        env["task"] = task
+        print(json.dumps(env))
     else:
-        env['task']['type'] = "worker"
-        env['task']["index"] = str(int(os.environ['BACKENDAI_CLUSTER_IDX']) - 1) # Index starts from 0
-    print(json.dumps(env))
-else:
-    print("")
+        print("")
