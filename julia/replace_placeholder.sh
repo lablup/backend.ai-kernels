eaa401ae7158112e0f97a49c1707b9eb3ff65732
Jeongkyu Shin <inureyes@gmail.com>
2023-01-27 17:42:55 +0900

    update: placeholder login to be more general

--- julia/replace_placeholder.sh
+++ julia/replace_placeholder.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 path="/opt/julia/packages/IJulia"
 first_dir=$(ls -1 "$path" | head -1)
-file="/usr/local/share/jupyter/kernels/julia-1.8/kernel.json"
+if [ -z "$1" ]
+then
+    version="1.5"
+else
+    version=$1
+fi
+file="/usr/local/share/jupyter/kernels/julia-${version}/kernel.json"
 old_string="PLACEHOLDER"
 sed -i "s/$old_string/$first_dir/g" $file
