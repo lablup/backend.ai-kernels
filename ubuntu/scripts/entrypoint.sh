500bbac13b7312c22a2d8588898727e3a2cee36e
Jungseung Yang <xyloon@gmail.com>
2023-01-31 00:26:25 +0900

    Supporting for execution on the Backend.AI

--- ubuntu/scripts/entrypoint.sh
+++ ubuntu/scripts/entrypoint.sh
@@ -5,9 +5,14 @@ trap ctrl_c INT
 function ctrl_c() {
   exit 0
 }
+
+[ ! -z "${RESOLUTION+x}" ] && export VNC_RESOLUTION="$RESOLUTION"
+[ ! -d "/home/work/.config" ] && mkdir /home/work/.config
+[ ! -d  "/home/work/.config/xfce4" ] && tar xzvf /usr/local/xfcedeskconfig/deskconfig.tar.gz -C /home/work/.config/ 
+
 # entrypoint.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server.
 rm /tmp/.X1-lock 2> /dev/null &
-/opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &
+/opt/noVNC/utils/launch.sh --vnc localhost:$VNC_PORT --listen $1 &
 # Insecure option is needed to accept connections from the docker host.
 vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
 wait
